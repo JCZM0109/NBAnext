4138dc1a3cd5e89f0f101204d8a6b57780de8f76
xOpusMagnumx <zambrano.meza901@gmail.com>
2023-08-09 19:39:09 -0500

    Added the team logo grid to the drawer's content, conditioned by the selected conf

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 
+
 import { AspectRatio, Box, Container, Flex, Heading, Text } from '@chakra-ui/react'
 import Image from 'next/image'
 import { ConfeSelection } from '@/components/ConfeSelection'
+
 
 
 export default function Home() {
@@ -21,17 +23,17 @@ export default function Home() {
             <Heading fontSize={{ base: '2xl', lg: '5xl' }} color="white">Welcome to JZ's corner</Heading>
           </Flex>
           <Flex justifyContent="space-around" alignItems="center" mt={{ base: "10px", lg: "20px" }}>
-            <Text fontSize={{ base: 'sm', lg: '2xl' }} w={{ base: "200px", lg: "600px" }} p="10px">
-              Welcome to my site! Made primarily to practice, not gonna lie. I will be getting data from a NBA API to show some of the teams' general info. Hope you enjoy!
+            <Text fontSize={{ base: 'sm', lg: '2xl' }} w={{ base: "200px", lg: "600px" }} p="10px" color="white" align={"justify"}>
+              Welcome to my site! Made primarily to practice, not gonna lie. I will be getting data from a NBA API to show some of the teams' general info. Click any of the conference logos to select your team!
             </Text>
             <Box w={{ base: "80px", lg: "100px" }} h="auto">
-              <AspectRatio ratio={9/16} position="relative">
-              <Image src="/images/SVG-nba.svg" fill={true} style={{objectFit: "contain"}} alt="NBA logo" />
+              <AspectRatio ratio={9 / 16} position="relative">
+                <Image src="/images/SVG-nba.svg" fill={true} style={{ objectFit: "contain" }} alt="NBA logo" />
               </AspectRatio>
             </Box>
           </Flex>
         </Container>
-        <ConfeSelection/>
+        <ConfeSelection />
       </main>
     </>
   )
