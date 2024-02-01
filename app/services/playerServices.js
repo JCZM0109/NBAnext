d1e9b28adc50ef82a63ad34fbabe613211da343a
xOpusMagnumx <zambrano.meza901@gmail.com>
2024-02-01 13:02:50 -0500

    Cleanup

--- app/services/playerServices.js
+++ app/services/playerServices.js
@@ -1,8 +1,24 @@
 import axios from "axios";
 
-export const getAllPlayers = async () => {
+
+
+export const getAllPlayers = async (team, season) => {
     
+
+    const options = {
+        method: 'GET',
+        url: 'https://api-nba-v1.p.rapidapi.com/players',
+        params: {
+          team: {team},
+          season: {season}
+        },
+        headers: {
+          'X-RapidAPI-Key': 'db53ce0518msh9435c1b8c9acd47p11798fjsn2e96c9776efb',
+          'X-RapidAPI-Host': 'api-nba-v1.p.rapidapi.com'
+        }
+      };
+
     const response = await axios.get("https://www.balldontlie.io/api/v1/players");
 
-    return response.data.data
+    return response.data
 };
